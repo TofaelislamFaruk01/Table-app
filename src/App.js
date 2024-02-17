1523f14d7c54e95dce4c6b07a329366521992088
TofaelislamFaruk01 <tofaelfaruk@gmail.com>
2024-02-17 23:28:27 +0600

    tableItems added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { useState } from 'react'
 import './App.css';
 import Header from './components/Header/Header';
 
+
+function App() {
+
+  // initialize all the menu item into true
+
+  const [tileopen, setTileopen] = useState(true);
+  const [priceOpen, setPriceOpen] = useState(true);
+  const [categoryOpen, setCategoryOpen] = useState(true);
+  const [dateOpen, setDateOpen] = useState(true);
+  const [authorOpen, setAuthorOpen] = useState(true);
+  const [stausOpen, setStausOpen] = useState(true);
+  const [actionOpen, setActionOpen] = useState(true);
+
+  
+
+  //crate an object using table information
 const  tableData=[
   {
     tile:{
@@ -16,6 +33,7 @@ const  tableData=[
     },
     author:"Admin",
     staus:'Published',
+    action:'Edit',
 
   },
   {
@@ -30,23 +48,128 @@ const  tableData=[
     },
     author:"Admin",
     staus:'Draft',
+    action:'Edit',
   }
 ]
 
-console.log(tableData);
-
-
-function App() {
+//console.log(tableData);
+  
   return (
    
-    <div>
-      <Header>
+    <>
+    
+      {/* header file */}
+      <Header></Header>
+   
+      
 
-      </Header>
+      {/* Table header part */}
+
+      <div>
+        <div className='tableItems'>
+              
+              {/* check tittle states are available or not */}
+
+          { tileopen &&
+            <div className='tableCard'>
+              <h4>Title</h4>
+            </div>   
+          }
+            {priceOpen &&
+            <div  className='tableCard'>
+              <h4>Price</h4>
+            </div>   
+          }
+            { categoryOpen &&
+            <div  className='tableCard'>
+              <h4>Category</h4>
+            </div>   
+          }
+            {dateOpen &&
+            <div  className='tableCard'>
+              <h4>Date</h4>
+            </div>   
+          }
+            { authorOpen&&
+            <div  className='tableCard'>
+              <h4>Author</h4>
+            </div>   
+          }
+            { stausOpen&&
+            <div  className='tableCard'>
+              <h4>Status</h4>
+            </div>   
+          }
+            {actionOpen &&
+            <div  className='tableCard'>
+              <h4>Action</h4>
+            </div>   
+          }
+
+        </div>
+
+          
+          {/* mapping each values in the array */}
+         {
+          tableData?.map((item,index)=>   <div key={index} className='tableItems'>
+
+        
+          { tileopen &&
+            <div  className='tableCard'>
+
+              {/* check the items and print*/}
+              <h4>{item?.tile?.title} <span>Ic</span> </h4> 
+              { item?.tile?.topic &&
+                <p>Topic: {item?.tile?.topic}</p>
+              }
+              { item?.tile?.lesson  &&
+                <p>Lesson: {item?.tile?.lesson}</p>
+              }
+            </div>   
+          }
+            {priceOpen&&
+            <div  className='tableCard'>
+              <h4>{item?.price}</h4>
+            </div>   
+          }
+            { categoryOpen &&
+            <div  className='tableCard'>
+              <h4>{item?.category}</h4>
+            </div>   
+          }
+            {dateOpen &&
+            <div  className='tableCard'>
+              <h4>{item?.date?.date}</h4>
+              <h4>{item?.date?.time}</h4>
+            </div>   
+          }
+            { authorOpen&&
+            <div  className='tableCard'>
+              <h4>{item?.author}</h4>
+            </div>   
+          }
+            { stausOpen&&
+            <div  className='tableCard'>
+              <h4>{item?.staus}</h4>
+            </div>   
+          }
+            {actionOpen &&
+            <div  className='tableCard'>
+              <h4>{item?.action}</h4>
+            </div>   
+          }
+
+        </div>
+        )} 
+        
+      </div>
+      
+    </>
+    
 
     
       
-    </div>
+  
   
 
     
